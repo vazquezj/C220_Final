268e74f307bf6ef6e008040158c475dad261f0b1
JORGEVAZQUEZ\Jorge <vazquezj@indiana.edu>
2017-12-10 20:38:54 -0500

    12-10-17 100% fully functional

--- Score.gd
+++ Score.gd
@@ -1,7 +1,7 @@
 extends Label
 
 var Score = 0
-var scene = 1
+export var scene = 1
 
 func increase_score():
 	self.Score += 1
@@ -12,15 +12,16 @@ func decrease_score():
 	self.text = "Score: " + str(Score)
 
 func _process(delta):
+	print (scene)
 	if self.Score < 0:
 		self.Score = 0
 	if self.Score >= 5 and scene == 1:
-		scene += 1
+		scene = 2
 		get_tree().change_scene("res://Scene2.tscn")
-	if self.Score >= 5 and scene == 2:
-		scene += 1
+	elif self.Score >= 5 and scene == 2:
+		scene = 3
 		get_tree().change_scene("res://Scene3.tscn")
-	if self.Score >= 5 and scene == 3:
+	elif self.Score >= 5 and scene == 3:
 		$"../WinScreen".visible = true
 	if Input.is_action_just_pressed("restart") and $"../WinScreen".visible == true:
-		get_tree().reload_current_scene()
+		get_tree().change_scene("res://Main.tscn")
