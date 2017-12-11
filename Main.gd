5003a71d47e2684ac8f630a5be8421e4b0ff7908
JORGEVAZQUEZ\Jorge <vazquezj@indiana.edu>
2017-12-10 20:04:44 -0500

    12-10-17 Random animal spawning is working.

--- Main.gd
+++ Main.gd
@@ -1,31 +1,35 @@
 extends Node
 
-func _on_RabbitTimer_timeout():
-	var newRabbit = $"Rabbit".duplicate()
-	self.add_child(newRabbit)
-	newRabbit.position.x = rand_range(450, 1470)
-	newRabbit.position.y = -100
-
-func _on_GiraffeTimer_timeout():
-	var newGiraffe = $"Giraffe".duplicate()
-	self.add_child(newGiraffe)
-	newGiraffe.position.x = rand_range(450, 1470)
-	newGiraffe.position.y = -100
+func _on_Timer_timeout():
+	var newAnimal = floor(rand_range(0, 4))
+	print (newAnimal)
+	if newAnimal == 0:
+		var newRabbit = $"Rabbit".duplicate()
+		self.add_child(newRabbit)
+		newRabbit.position.x = rand_range(200, 1080)
+		newRabbit.position.y = -100
+	if newAnimal == 1:
+		var newGiraffe = $"Giraffe".duplicate()
+		self.add_child(newGiraffe)
+		newGiraffe.position.x = rand_range(200, 1080)
+		newGiraffe.position.y = -100
+	if newAnimal == 2:
+		var newMonkey = $"Monkey".duplicate()
+		self.add_child(newMonkey)
+		newMonkey.position.x = rand_range(200, 1080)
+		newMonkey.position.y = -100
+	if newAnimal == 3:
+		var newHippo = $"Hippo".duplicate()
+		self.add_child(newHippo)
+		newHippo.position.x = rand_range(200, 1080)
+		newHippo.position.y = -100
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_up"):
-		var newUpWall = $"UpWall".duplicate()
-		self.add_child(newUpWall)
-		newUpWall.position = get_viewport().get_mouse_position()
+		$"UpWall".position = get_viewport().get_mouse_position()
 	if Input.is_action_just_pressed("ui_down"):
-		var newDownWall = $"DownWall".duplicate()
-		self.add_child(newDownWall)
-		newDownWall.position = get_viewport().get_mouse_position()
+		$"DownWall".position = get_viewport().get_mouse_position()
 	if Input.is_action_just_pressed("ui_left"):
-		var newLeftWall = $"LeftWall".duplicate()
-		self.add_child(newLeftWall)
-		newLeftWall.position = get_viewport().get_mouse_position()
+		$"LeftWall".position = get_viewport().get_mouse_position()
 	if Input.is_action_just_pressed("ui_right"):
-		var newRightWall = $"RightWall".duplicate()
-		self.add_child(newRightWall)
-		newRightWall.position = get_viewport().get_mouse_position()
+		$"RightWall".position = get_viewport().get_mouse_position()
--- Score.gd
+++ Score.gd
@@ -11,9 +11,10 @@ func decrease_score():
 	self.text = "Score: " + str(Score)
 
 func _process(delta):
-	if self.Score <= 0:
+	if self.Score < 0:
 		self.Score = 0
 	if self.Score >= 5:
+		#get_tree().change_scene("res://myNextScene.tscn")
 		$"../WinScreen".visible = true
 	if Input.is_action_just_pressed("restart") and $"../WinScreen".visible == true:
 		get_tree().reload_current_scene()
