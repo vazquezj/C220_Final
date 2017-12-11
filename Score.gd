2d1774c1ffb3318eb4d26c8213a0290b6c161876
JORGEVAZQUEZ\Jorge <vazquezj@indiana.edu>
2017-12-10 20:20:27 -0500

    12-10-17 100% fully functional - scene switching issue

--- Score.gd
+++ Score.gd
@@ -1,6 +1,7 @@
 extends Label
 
 var Score = 0
+var scene = 1
 
 func increase_score():
 	self.Score += 1
@@ -13,8 +14,13 @@ func decrease_score():
 func _process(delta):
 	if self.Score < 0:
 		self.Score = 0
-	if self.Score >= 5:
-		#get_tree().change_scene("res://myNextScene.tscn")
+	if self.Score >= 5 and scene == 1:
+		scene += 1
+		get_tree().change_scene("res://Scene2.tscn")
+	if self.Score >= 5 and scene == 2:
+		scene += 1
+		get_tree().change_scene("res://Scene3.tscn")
+	if self.Score >= 5 and scene == 3:
 		$"../WinScreen".visible = true
 	if Input.is_action_just_pressed("restart") and $"../WinScreen".visible == true:
 		get_tree().reload_current_scene()
